de73c182b0e73d8a8d210435941bcc8891a301f3
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-13 16:18:50 +1000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,20 @@
 module.exports = (client, member) => {
 
-  const serverConfig = client.getGuildSettings(member.guild);
+  client.serverConfig.ensure(member.guild.id, client.config.defaultConfig);
   
-  if (serverConfig.announceNewMembers !== "true") return;
+  let announcementCheck = client.serverConfig.get(member.guild.id, "announceNewMembers");
   
-  const welcomeMessage = serverConfig.newMemberAnnouncementMessage.replace("{{member}}", member.user.tag);
+  if (announcementCheck !== "true") return;
   
-  member.guild.channels.find("name", serverConfig.newMemberAnnouncementChannel).send(welcomeMessage).catch(console.error);
+  
+  let welcomeMessage = client.serverConfig.get(member.guild.id, "newMemberAnnouncementMessage"); 
+  
+  // Our welcome message has a bit of a placeholder, let's fix that:
+  welcomeMessage = welcomeMessage.replace("{{member}}", member.user.tag)
+  
+  // we'll send to the welcome channel.
+  member.guild.channels
+    .find("name", client.serverConfig.get(member.guild.id, "newMemberAnnouncementChannel"))
+    .send(welcomeMessage)
+    .catch(console.error);
 };
