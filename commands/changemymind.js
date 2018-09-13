52238c5298ec03fcce0cec1a0ef344114b8d282e
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-13 17:58:59 +1000

    Update changemymind.js

--- commands/changemymind.js
+++ commands/changemymind.js
@@ -2,13 +2,19 @@ exports.run = async (client, message, args, level) => {
 
     const { MessageAttachment } = require("discord.js");
     const input = args.join(" ");
+    const url = await `https://logan-bot-api.herokuapp.com/commands/changemymind/${input}`;
     
     message.channel.startTyping();
     
-    const attachment = new MessageAttachment("https://logan-bot-api.herokuapp.com/commands/changemymind/" + input);
+    const attachment = new MessageAttachment(url);
     message.channel.send("Nice Memeing!~", attachment);
     
+    message.channel.stopTyping();
     
+    if (err) {
+        message.channel.send("There was an error creating the image, sorry ;-;");
+        message.channel.stopTyping();
+    }
 };
 
 exports.conf = {
