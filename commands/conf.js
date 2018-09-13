ee02b06e2563b915c48f18e45d85642942dbf1c1
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-13 18:37:29 +1000

    Update conf.js

--- commands/conf.js
+++ commands/conf.js
@@ -57,8 +57,8 @@ exports.run = async (client, message, [action, key, ...value], level) => {
     
     const longest = Object.keys(serverConfig).reduce((long, str) => Math.max(long, str.length), 0);
     var confParser = `[The following are the servers current configuration items:\n`;
-    Object.keys(serverConfig).map(prop => {
-      confParser +=`${prop}${' '.repeat(longest - props.length)}:  ${serverConfig[prop]}\n`;
+    Object.keys(serverConfig).forEach( prop => {
+      confParser += `${prop}${" ".repeat(longest - prop.length)}:  ${serverConfig[prop]}\n`;
     })
 
     confParser += `Use ${client.config.prefix}conf edit [key] [newValue] to change a configuration item,\nor reset an item with ${client.config.prefix}conf reset [key]`;
