54c171964941f7285f89830f59c7233bec7dcb0c
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-10-14 18:08:45 +1100

    Update burnpaper.js

--- commands/burnpaper.js
+++ commands/burnpaper.js
@@ -1,62 +1,62 @@
 exports.run = async (client, message, args, level) => {
 
-  const Discord = require("discord.js");
-  const fs = require('fs')
-  const path = require('path')
-  const Canvas = require('canvas')
+const Discord = require("discord.js");
+const fs = require('fs')
+const path = require('path')
+const Canvas = require('canvas')
 
-  const Image = Canvas.Image
-  const canvas = Canvas.createCanvas(622, 727)
-  const ctx = canvas.getContext('2d')
+const Image = Canvas.Image
+const canvas = Canvas.createCanvas(622,727)
+const ctx = canvas.getContext('2d')
 
   function wrapText(context, text, x, y, maxWidth, lineHeight) {
-    var words = text.split(' ');
-    var line = '';
+        var words = text.split(' ');
+        var line = '';
 
-    for (var n = 0; n < words.length; n++) {
-      var testLine = line + words[n] + ' ';
-      var metrics = context.measureText(testLine);
-      var testWidth = metrics.width;
-      if (testWidth > maxWidth && n > 0) {
+        for(var n = 0; n < words.length; n++) {
+          var testLine = line + words[n] + ' ';
+          var metrics = context.measureText(testLine);
+          var testWidth = metrics.width;
+          if (testWidth > maxWidth && n > 0) {
+            context.fillText(line, x, y);
+            line = words[n] + ' ';
+            y += lineHeight;
+          }
+          else {
+            line = testLine;
+          }
+        }
         context.fillText(line, x, y);
-        line = words[n] + ' ';
-        y += lineHeight;
       }
-      else {
-        line = testLine;
-      }
-    }
-    context.fillText(line, x, y);
-  }
+      
+const img = new Image()
 
-  const img = new Image()
+img.onload = () => {
+  
+ctx.drawImage(img, 0, 0)
+  
+ctx.font = 'normal 20px Verdana'
+  
+ctx.fillStyle = '#000'
 
-  img.onload = () => {
+var maxWidth = 210;
+var lineHeight = 20;
+var x = 49;
+var y = 99;
+var text = args.join(" ");
+wrapText(ctx, text, x, y, maxWidth, lineHeight); 
 
-    ctx.drawImage(img, 0, 0)
-
-    ctx.font = 'normal 20px Verdana'
-
-    ctx.fillStyle = '#000'
-
-    var maxWidth = 210;
-    var lineHeight = 20;
-    var x = 49;
-    var y = 99;
-    var text = args;
-    wrapText(ctx, text, x, y, maxWidth, lineHeight);
-
-    var m = ctx.measureText(args)
+var m = ctx.measureText(args.join(" "));
 
 
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'quid.png');
-    message.channel.send('You burn that paper!', attachment);
-  }
-  img.onerror = err => {
-    console.log(err)
-  }
-  img.src = 'https://i.imgur.com/n5JdAAs.jpg'
-
+  const attachment = new Discord.Attachment(canvas.toBuffer(), 'quid.png');
+  message.channel.send('You burn that paper!', attachment);
+}
+img.onerror = err => {
+  console.log(err)
+}
+img.src = 'https://i.imgur.com/n5JdAAs.jpg'
+  
 };
 
 exports.conf = {
