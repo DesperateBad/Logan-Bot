8678fcd70102d4c1a84634a053f1d29fdfb7b72c
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-10-14 18:08:11 +1100

    Update conf.js

--- commands/conf.js
+++ commands/conf.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, [action, key, ...value], level) => {
   // Retrieve current guild settings (merged) and overrides only.
   const serverConfig = message.serverConfig;
   const overrides = client.serverConfig.get(message.guild.id);
-
+  
   // Edit an existing key value
   if (action === "edit") {
     // User must specify a key.
@@ -18,7 +18,7 @@ exports.run = async (client, message, [action, key, ...value], level) => {
     if (value.join(" ") === serverConfig[key]) return message.reply("Are you really trying to change that key's value to its current value...?");
 
     if (!client.serverConfig.has(message.guild.id)) client.serverConfig.set(message.guild.id, {});
-
+    
     // setProp is an enmap feature, it defines a single property of an object in an enmap key/value pair.
     client.serverConfig.setProp(message.guild.id, key, value.join(" "));
 
@@ -26,44 +26,44 @@ exports.run = async (client, message, [action, key, ...value], level) => {
     console.log(`${key} was edited to ${value.join(" ")} in guild: ${message.guild.name}`);
     message.reply(`${key} successfully edited to ${value.join(" ")}`);
   } else
+  
+  // Resets a key to the default value
+  if (action === "reset") {
+    if (!key) return message.reply("Please specify a key to reset");
+    if (!serverConfig[key]) return message.reply("That key does not exist in my settings!");
+    if (!overrides[key]) return message.reply("That key does not have an override and is already using its default value");
+    
+    // Good demonstration of the custom awaitReply method in `./modules/functions.js` !
+    const response = await client.awaitReply(message, `Are you sure you want to reset ${key} to the default value?\n**Respond with Yes/No**`);
 
-    // Resets a key to the default value
-    if (action === "reset") {
-      if (!key) return message.reply("Please specify a key to reset");
-      if (!serverConfig[key]) return message.reply("That key does not exist in my settings!");
-      if (!overrides[key]) return message.reply("That key does not have an override and is already using its default value");
-
-      // Good demonstration of the custom awaitReply method in `./modules/functions.js` !
-      const response = await client.awaitReply(message, `Are you sure you want to reset ${key} to the default value?\n**Respond with Yes/No**`);
-
-      // If they respond with y or yes, continue.
-      if (["y", "yes"].includes(response.toLowerCase())) {
-        // We delete the `key` here.
-        delete overrides[key];
-        client.serverConfig.set(message.guild.id, overrides);
-        message.reply(`${key} was successfully reset!`);
-      } else
-        // If they respond with n or no, we inform them that the action has been cancelled.
-        if (["n", "no", "cancel"].includes(response)) {
-          message.reply("Action cancelled.");
-        }
+    // If they respond with y or yes, continue.
+    if (["y", "yes"].includes(response.toLowerCase())) {
+      // We delete the `key` here.
+      delete overrides[key];
+      client.serverConfig.set(message.guild.id, overrides);
+      message.reply(`${key} was successfully reset!`);
     } else
-
-      if (action === "view") {
-        if (!key) return message.reply("Are you trying to view nothing?");
-        if (!serverConfig[key]) return message.reply("That key does not exist in my settings");
-        const isDefault = !overrides[key] ? "\nThis is the global default value." : "";
-        message.reply(`The value of ${key} in my settings is currently ${serverConfig[key]}${isDefault}`);
-      } else {
-
-        const longest = Object.keys(serverConfig).reduce((long, str) => Math.max(long, str.length), 0);
-        var confParser = `The following are the servers current configuration items:\n-----\n`;
-        Object.keys(serverConfig).forEach(prop => {
-          confParser += `${prop}${" ".repeat(longest - prop.length)}::  ${serverConfig[prop]}\n`;
-        })
-
-        message.channel.send(confParser, { code: "asciidoc", split: { char: "\u200b" } });
-      }
+    // If they respond with n or no, we inform them that the action has been cancelled.
+    if (["n","no","cancel"].includes(response)) {
+      message.reply("Action cancelled.");
+    }
+  } else
+  
+  if (action === "view") {
+    if (!key) return message.reply("Are you trying to view nothing?");
+    if (!serverConfig[key]) return message.reply("That key does not exist in my settings");
+    const isDefault = !overrides[key] ? "\nThis is the global default value." : "";
+    message.reply(`The value of ${key} in my settings is currently ${serverConfig[key]}${isDefault}`);
+  } else {
+    
+    const longest = Object.keys(serverConfig).reduce((long, str) => Math.max(long, str.length), 0);
+    var confParser = `The following are the servers current configuration items:\n-----\n`;
+    Object.keys(serverConfig).forEach( prop => {
+      confParser += `${prop}${" ".repeat(longest - prop.length)}::  ${serverConfig[prop]}\n`;
+    })
+    
+    message.channel.send(confParser, {code: "asciidoc", split: { char: "\u200b" }}).then(msg => { msg.delete(15000) });
+   }
 };
 
 exports.conf = {
